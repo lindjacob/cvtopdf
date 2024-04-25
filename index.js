e7cf9fbeeb238359854b8b2e572fef27cc093861
@au340161 <201304090@post.au.dk>
2024-04-25 17:33:25 +0200

    add auth + cors

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 import http from 'http';
 import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const server = http.createServer(async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://jacoblind.me');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle pre-flight requests
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+  
+  const auth = { 
+    login: process.env.BASIC_AUTH_USERNAME, 
+    password: process.env.BASIC_AUTH_PASSWORD 
+  };
+
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  if (!login || !password || login !== auth.login || password !== auth.password) {
+    res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="401"' });
+    res.end('Access denied');
+    return;
+  }
+
   if (req.url === '/generate-pdf') {
     try {
       const browser = await puppeteer.launch({
@@ -28,10 +57,7 @@ const server = http.createServer(async (req, res) => {
 
       // Send the PDF buffer to the client and close the server after sending
       res.end(pdfBuffer, () => {
-        console.log('PDF sent to client, closing server.');
-        server.close(() => {
-          console.log('Server closed.');
-        });
+        console.log('PDF sent to client.');
       });
     } catch (error) {
       console.error(error);
